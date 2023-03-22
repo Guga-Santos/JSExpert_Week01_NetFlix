20373bc0b9c4530f116abd8eaf83a86a4c745be6
GugaSantos <guga.santos.guga@gmail.com>
2023-03-22 10:12:30 -0300

    chore: add a code on script to generate 720 version of files

--- script.sh
+++ script.sh
@@ -10,5 +10,29 @@ for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
   OUTPUT=$ASSETSFOLDER/$FILENAME/$FILENAME
   DURATION=$(ffprobe -i $INPUT -show_format -v quiet | sed -n 's/duration=//p')
 
-  echo $DURATION
+  OUTPUT144=$OUTPUT-$DURATION-144
+  OUTPUT360=$OUTPUT-$DURATION-360
+  OUTPUT720=$OUTPUT-$DURATION-720
+
+  echo 'rendering in 720'
+  
+  # a flag -y é pra sobrescrever se já houver algum arquivo na pasta de saída
+  # -c:a é o canal de audio e o -ac se refere a quantos canais serão.
+  # -vcodec e -acodec é codec de vídeo e codec de audio
+  # -ab é o avarage bitRate
+  # movflag é a flag muito doida
+  # -b:v é o bitRate (velocidade de download)
+  # bufsize é o buffer size (tamanho de cada pedaço que será picotado)
+  # -vf é o value filter
+
+  ffmpeg -y -i $INPUT \
+    -c:a aac -ac 2 \
+    -vcodec h264 -acodec aac \
+    -ab 128k \
+    -movflags frag_keyframe+empty_moov+default_base_moof \
+    -b:v 1500k \
+    -maxrate 1500k \
+    -bufsize 1000k \
+    -vf "scale=-1:720" \
+    $OUTPUT720.mp4
 done
