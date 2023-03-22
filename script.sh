6721129afdb42e6e66bc8710c358acb898fe6ee9
GugaSantos <guga.santos.guga@gmail.com>
2023-03-22 09:43:51 -0300

    chore: add code to create directory for each file

--- script.sh
+++ script.sh
@@ -2,5 +2,7 @@ ASSETSFOLDER=assets/timeline
 for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
   # cortar a extensão e a resolução do arquivo
   FILENAME=$(echo $mediaFile | sed -n 's/.mp4//p' | sed -n 's/-1920x1080//p')
-  echo $FILENAME
+  INPUT=$ASSETSFOLDER/$mediaFile
+  FOLDER_TARGET=$ASSETSFOLDER/$FILENAME
+  mkdir -p $FOLDER_TARGET
 done
