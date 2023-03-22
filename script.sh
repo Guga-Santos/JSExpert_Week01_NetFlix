6c88deef15df16537c2afae73a85a985de1b9d63
GugaSantos <guga.santos.guga@gmail.com>
2023-03-22 09:40:39 -0300

    chore: adding code to edit files name

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 ASSETSFOLDER=assets/timeline
 for mediaFile in `ls $ASSETSFOLDER | grep .mp4`; do
-    echo $mediaFile
+  # cortar a extensão e a resolução do arquivo
+  FILENAME=$(echo $mediaFile | sed -n 's/.mp4//p' | sed -n 's/-1920x1080//p')
+  echo $FILENAME
 done
