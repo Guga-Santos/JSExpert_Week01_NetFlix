15d693d598de476998b4090590af7938087fd280
GugaSantos <guga.santos.guga@gmail.com>
2023-03-22 23:58:16 -0300

    create main function and initialize a new videoComponent

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -2,8 +2,13 @@ const MANIFEST_URL = 'manifest.json';
 const localHost = ['127.0.0.1', 'localhost'];
 
 async function main() {
-    
+    const isLocal = !!~localHost.indexOf(window.location.hostname);
+    console.log('isLocal?: ', isLocal)
+    const manifestJSON = await (await fetch(MANIFEST_URL)).json();
+    const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost;
+    const videoComponent = new VideoComponent();
 
+    videoComponent.initializePlayer();
 }
 
 window.onload = main
