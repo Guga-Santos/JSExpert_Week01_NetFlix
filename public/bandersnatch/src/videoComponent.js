4ae3af1f17c9cfd372c62ad12927c0ae9e40c4ed
GugaSantos <guga.santos.guga@gmail.com>
2023-03-23 03:38:49 -0300

    feat: create getModalTemplate

--- public/bandersnatch/src/videoComponent.js
+++ public/bandersnatch/src/videoComponent.js
@@ -1,6 +1,6 @@
 class VideoComponent {
   constructor() {
-
+    this.modal = {};
   }
   initializePlayer() {
     const player = videojs('vid');
@@ -11,6 +11,34 @@ class VideoComponent {
     });
 
     player.addChild(modal);
+
+    player.on('play', () => modal.close())
+
+    this.modal = modal;
+  }
+
+  getModalTemplate(options, modal) {
+    return (_) => {
+      const [ option1, option2 ] = options;
+      const htmlTemplate = `
+      <div class='overlay'>
+        <div class='videoButtonWrapper'>
+          <button 
+          type="button" 
+          class="btn btn-dark"
+          onClick="window.nextchunk('${option1}')>
+          ${option1}
+          </button>
+          <button 
+          type="button" 
+          class="btn btn-dark"
+          onClick="window.nextchunk('${option2}')>
+          ${option2}
+          </button>
+        </div>
+      </div>
+      `
+    }
   }
 
 }
