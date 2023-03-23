22b594b7b7bd1f67916b7b36df35ca60273db207
GugaSantos <guga.santos.guga@gmail.com>
2023-03-22 23:09:54 -0300

    feat: create videoComponent class

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -1,12 +1,5 @@
 async function main() {
-    const player = videojs('vid');
-    const ModalDialog = videojs.getComponent('ModalDialog');
-    const modal = new ModalDialog(player, {
-        temporary: false, 
-        closeable: true
-    });
-
-    player.addChild(modal);
+    
 
 }
 
