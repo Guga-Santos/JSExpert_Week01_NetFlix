00fa11f973393595fb80454701cf227e4c274433
GugaSantos <guga.santos.guga@gmail.com>
2023-03-23 00:26:28 -0300

    feat: create sourceOpenWrapper method on VideoMediaPlayer class

--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -3,6 +3,7 @@ class VideoMediaPlayer {
     this.manifestJSON = manifestJSON;
     this.videoElement = null;
     this.sourceBuffer = null;
+    this.selected = {};
   }
 
   initializeCodec() {
@@ -16,6 +17,23 @@ class VideoMediaPlayer {
     const codecSupported = MediaSource.isTypeSupported(this.manifestJSON.codec);
     if(!codecSupported) {
       alert(`This browser do not support the codec: ${this.manifestJSON.codec}` );
+      return;
+    }
+
+    const mediaSource = new MediaSource();
+    this.videoElement.src = URL.createObjectURL(mediaSource);
+
+    mediaSource.addEventListener("sourceopen", this.sourceOpenWrapper(mediaSource))
+
+  }
+
+  sourceOpenWrapper(mediaSource) {
+    return async(_) => {
+      this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec);
+      const selected = this.selected = this.manifestJSON.intro
+      //Evita rodar como LIVE
+      mediaSource.duration = 0;
+
     }
   }
 }
